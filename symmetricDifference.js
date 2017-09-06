5aa98552dc0e78b9f6e177ee124ce7ef2b63b5a1
Ara <ayaghsizian@gmail.com>
2017-09-05 20:41:38 -0400

    used uniq function which uses Set object to eliminate dupes

--- symmetricDifference.js
+++ symmetricDifference.js
@@ -1,34 +1,53 @@
-function sym(input) {
+function sym(args) {
 
+  // found this unique function online to eliminate duplicate numbers in each argument
+  var uniq = function(a){
+    return Array.from(new Set(a));
+  };
   var answer = [];
   var longArray = [];
 
-  for(var i = 0; i < input.length - 1; i++){
-    var arg0 = [].slice.call(arguments[i]);
-    var arg1 = [].slice.call(arguments[i+1]);
-
-    for(var j = 0; j < arg0.length; j++){
-
-      if( !arg1.includes( arg0[j] ) ){
-        answer.push( arg0[j] );
-      }//if
-    }//for j
-
-    for(var j = 0; j < arg1.length; j++){
-      if( !arg0.includes( arg1[j] ) ){
-        answer.push( arg1[j] );
-      }//if
-    }//for j
-
-  }//for i
-
-  console.log("arguments.length: "+args);
+  // the main for loop through unknown number of arguments
+  for(var i = 0; i < arguments.length; i++){
+    if(i == 0){
+      longArray = longArray.concat(uniq(arguments[i])).sort();
+    }// if 0
+    if(i == 1){
+        longArray = longArray.concat(uniq(arguments[i])).sort();
+        if(longArray[0] != longArray[1]){
+            answer.push(longArray[0]);
+        }//if
+        for(var j = 1; j < longArray.length - 1; j++){
+          if( (longArray[j] != longArray[j-1]) && (longArray[j] != longArray[j+1]) ){
+              answer.push(longArray[j]);
+          }//if
+        }// for j
+        if(longArray[longArray.length-1] != longArray[longArray.length-2]){
+          answer.push(longArray[longArray.length-1]);
+        }
+    }//if 1
+    if(i > 1){
+        longArray = answer.concat(uniq(arguments[i])).sort();
+        answer = [];
+        if(longArray[0] != longArray[1]){
+            answer.push(longArray[0]);
+        }//if
+        for(var j = 1; j < longArray.length - 1; j++){
+          if(longArray[j] != longArray[j-1]){
+            if(longArray[j] != longArray[j+1]){
+              answer.push(longArray[j]);
+            }
+          }
+        }// for j
+        if(longArray[longArray.length-1] != longArray[longArray.length-2]){
+          answer.push(longArray[longArray.length-1]);
+        }
+    }//if > 1
+  }//main for loop i
   return answer;
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
-
-// [1, 2, 3].includes(2);
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]);
 
 
 
@@ -37,7 +56,7 @@ sym([1, 2, 3], [5, 2, 1, 4]);
 
 
 
-// passes 5/9 tests but not duplicate numbers in same array 
+// passes 5/9 tests but not duplicate numbers in same array
 //
 // function sym(args) {
 //   var answer = [];
